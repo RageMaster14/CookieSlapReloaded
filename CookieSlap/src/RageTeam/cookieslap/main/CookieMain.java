8fa217e6bcd37d190c7940a2e932e16c5bdd3e81
TheMinersDomain <theminersdomain@live.com>
2013-07-28 17:05:32 -0500

    Made scoreboard, worked on main file.

--- CookieSlap/src/RageTeam/cookieslap/main/CookieMain.java
+++ CookieSlap/src/RageTeam/cookieslap/main/CookieMain.java
@@ -3,27 +3,30 @@ package RageTeam.cookieslap.main;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
 
 public class CookieMain extends JavaPlugin{
 	public final Logger logger = Logger.getLogger("Minecraft");
 
+	//Objects
 	public Object chat;
-
 	public Object playerManager;
-
 	public Object cfgManager;
+	
+	public static Scoreboard main;
+	
+	public static int players = 0;
+	public static int gameLoop;
 	
 	public static CookieMain plugin;
 	
-	public static int timeInSeconds;
-	
-	public static int gameLoop = 0;
-	
+
 	public static boolean canStart;
 	
 	@Override
@@ -38,19 +41,21 @@ public class CookieMain extends JavaPlugin{
 	public void onEnable(){
 		PluginDescriptionFile pdf = this.getDescription();
 		this.logger.info(pdf.getName() + " v" + pdf.getVersion() + " Has Been Enabled!");
-		timeInSeconds = 120;
 		canStart = false;
-		gameLoop = getServer().getScheduler().scheduleSyncRepeatingTask(this, new GameLoop(), 20L, 20L);
 	}
 	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event){
+		players ++;
+		Player player = event.getPlayer();
+		player.setScoreboard(main);
 		if(Bukkit.getOnlinePlayers().length >= 6)
 		canStart = true;
 	}
 	
 	@EventHandler
 	public void onPlayerLeave(PlayerQuitEvent event){
+		players --;
 		if(Bukkit.getOnlinePlayers().length - 1 >= 6)
 		canStart = false;
 	}
