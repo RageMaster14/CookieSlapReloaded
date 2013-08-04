59d913536a33c12826ea2a4632cdaaeee6096d5c
Sam <Sam@Sam-PC.home>
2013-08-04 12:09:51 -0400

    Made the scoreboard finally

--- CookieSlap/src/rageteam/cookieslap/main/CS.java
+++ CookieSlap/src/rageteam/cookieslap/main/CS.java
@@ -1,6 +1,15 @@
 package rageteam.cookieslap.main;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
 
 import rageteam.cookieslap.commands.HelpCommand;
 import rageteam.cookieslap.handlers.PlayerHandler;
@@ -36,10 +45,15 @@ public class CS extends JavaPlugin{
 	public Misc misc;
 	
 	//Scoreboard
-	public CookieSlapBoard csBoard;
+	ScoreboardManager manager;
+	Scoreboard board;
+	Objective obj;
+	public int timeInSeconds = 240;
+	public int onlinePlayers = Bukkit.getServer().getOnlinePlayers().length;
+	public int highScore = 0;
+	public int arenaID = 0;
 	
 	private void loadDependicies(){
-		this.csBoard = new CookieSlapBoard(this);
 		
 		this.chat = new Chat(this);
 		this.logger = new Logger(this);
@@ -71,9 +85,47 @@ public class CS extends JavaPlugin{
 		loadHandlers();
 		registerCommands();
 		
-		csBoard.enable();
-		
 		logger.log(false, "Loading CS Dependicies");
+		
+		//ScoreBoard
+		manager = Bukkit.getScoreboardManager();
+		board = manager.getNewScoreboard();
+		obj = board.registerNewObjective("CookieSlap", "dummy");
+		
+		obj.setDisplayName(ChatColor.GRAY +  "  /toggleboard  " + ChatColor.WHITE + " to hide");
+		obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+		
+		final Score time = obj.getScore(Bukkit.getOfflinePlayer(ChatColor.LIGHT_PURPLE + "Time Left:" + ChatColor.RED));
+		getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
+			public void run(){
+				if(timeInSeconds != -1){
+					if(timeInSeconds != 0){
+						time.setScore(timeInSeconds);
+					} else if(timeInSeconds <= 10 && timeInSeconds > 0){
+						note();
+					}
+				}
+			}
+		}, 0L, 20L);
+		
+		Score players = obj.getScore(Bukkit.getOfflinePlayer(ChatColor.DARK_AQUA + "Players:" + ChatColor.RED));
+		players.setScore(onlinePlayers);
+		
+		Score arena = obj.getScore(Bukkit.getOfflinePlayer(ChatColor.GREEN + "Arena ID:" + ChatColor.RED));
+		arena.setScore(arenaID);
+		
+		Score score = obj.getScore(Bukkit.getOfflinePlayer(ChatColor.YELLOW + "HighScore:" + ChatColor.RED));
+		score.setScore(highScore);
+		
+		for(Player player : Bukkit.getOnlinePlayers()){
+			player.setScoreboard(board);
+		}
+	}
+		
+		private void note() {
+			for(Player player : getServer().getOnlinePlayers()){
+				player.playSound(player.getLocation(), Sound.NOTE_PIANO, 10, 1);
+			}
 	}
 	
 	@Override
