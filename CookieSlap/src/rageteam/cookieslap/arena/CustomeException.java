40db2c8de313d5f98cae594620624952b4645c7d
Sam <Sam@Sam-PC.home>
2013-08-13 18:22:59 -0400

    Added Some Events, Aswell as database functions

---
+++ CookieSlap/src/rageteam/cookieslap/arena/CustomeException.java
@@ -0,0 +1,9 @@
+package rageteam.cookieslap.arena;
+
+public class CustomeException extends Exception{
+	private static final long serialVersionUID = 1L;
+
+	public CustomeException(String message) {
+		super(message);
+	}
+}
---
+++ CookieSlap/src/rageteam/cookieslap/events/ArenaStopEvent.java
@@ -0,0 +1,46 @@
+package rageteam.cookieslap.events;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.Plugin;
+
+import rageteam.cookieslap.arena.Arena;
+
+public class ArenaStopEvent {
+	private static final HandlerList handlers = new HandlerList();
+    private Arena arena = null;
+    private Plugin plugin = null;
+    private FileConfiguration arenaConfig = null;
+
+    public ArenaStopEvent(Arena arena, Plugin plugin, FileConfiguration arenaConfig)
+    {
+    	this.arena = arena;
+    	this.plugin = plugin;
+    	this.arenaConfig = arenaConfig;
+    }
+
+    public Arena getArena()
+    {
+    	return this.arena;
+    }
+
+    public Plugin getPlugin()
+    {
+    	return this.plugin;
+    }
+
+    public FileConfiguration getArenaConfig()
+    {
+    	return this.arenaConfig;
+    }
+
+    public HandlerList getHandlers()
+    {
+    	return handlers;
+    }
+
+    public static HandlerList getHandlerList()
+    {
+    	return handlers;
+    }
+}
